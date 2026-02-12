61c7e71394d0e9c77363db63bb2803e2c786984e
박진혁 <qkrwlsgurzk@naver.com>
2026-02-13 08:18:24 +0900

    주석 달기

--- 0212/1945. 간단한 소인수분해.py
+++ 0212/1945. 간단한 소인수분해.py
@@ -2,18 +2,18 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
 
-    prime_num = [2, 3, 5, 7, 11]
-    prime_dict = {}
+    prime_num = [2, 3, 5, 7, 11]                                            # 사용되는 소수 리스트 생성
+    prime_dict = {}                                                         # 소수의 개수(지수)를 담을 딕셔너리 생성
 
-    for i in prime_num:
-        while N % i == 0:
+    for i in prime_num:                                                     # 소수로 나눈 나머지가 0이 안나올 때까지 계속 나누기 
+        while N % i == 0:                                                   
             N = N//i
-            if i not in prime_dict:
+            if i not in prime_dict:                                         # 나눈 나머지가 0이되면 그 소수를 딕셔너리에 추가
                 prime_dict[i] = 1
             else:
                 prime_dict[i] += 1
 
-    answer = " ".join(str(prime_dict.get(p, 0)) for p in prime_num)
+    answer = " ".join(str(prime_dict.get(p, 0)) for p in prime_num)         # 딕셔너리의 밸류 값을 문자열로 나열
 
 
     print(f'#{tc} {answer}')
--- 0212/2805 농작물 수확하기.py
+++ 0212/2805 농작물 수확하기.py
@@ -2,17 +2,17 @@ T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    farm = [list(map(int, input())) for _ in range(N)]
+    farm = [list(map(int, input())) for _ in range(N)]          # 입력값 받기
 
-    sum_value = 0
+    sum_value = 0                                               # 값의 합 구하기(마름모로 순회만 할 줄 알면 여태까지 하던 것과 같음)
 
-    for i in range(N):
+    for i in range(N):                                          
 
-        if i <= N//2:
+        if i <= N//2:                                           # 행이 중앙보다 작거나 같을 때는 중앙을 기준으로 행 수 만큼 양쪽으로 범위 증가(중)
             for j in range((N//2)-i, (N//2)+i+1):
                 sum_value += farm[i][j]
 
-        if i > N//2:
+        if i > N//2:                                            # 행이 중앙보다 클 때에의 범위에 따라 감소
             for j in range(i-N//2, N - (i - N//2)):
                 sum_value += farm[i][j]
 
